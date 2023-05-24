6340d20752a289cddee94473be01c4491d315e93
reedlin2002 <10160393@me.mcu.edu.tw>
2023-05-24 12:11:25 +0800

    update

--- Final_project/main.cpp
+++ Final_project/main.cpp
@@ -5,26 +5,39 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * lowarmR = NULL;
 GLMmodel * uparmR = NULL;
+FILE * fout = NULL;
+FILE * fin = NULL;
+float teapotX=0, teapotY=0;
+float angle[] = {};
 int show[4] = {1,1,1,1};
 int ID=3;
+int oldX=0, oldY=0;
 void keyboard(unsigned char key,int x, int y)
 {
     if(key == '0') ID = 0;
     if(key == '1') ID = 1;
     if(key == '2') ID = 2;
     if(key == '3') ID = 3;
-    ///if(key=='0') show[0] = !show[0];
-    ///if(key=='1') show[1] = !show[1];
-    ///if(key=='2') show[2] = !show[2];
-    ///if(key=='3') show[3] = !show[3];
+    if(key == 's')
+    {
+        if(fout==NULL) fout = fopen("motion.txt","w");
+        for (int i=0; i<20; i++)
+        {
+            fprintf(fout, ".%2f ",angle[i]);
+        }
+        fprintf(fout,"\n");
+    }
+    else if(key == 'r')
+    {
+        if(fin==NULL) fin = fopen("motion.txt","r");
+        for(int i=0; i<20; i++)
+        {
+            fscanf(fin, "%f", &angle[i]);
+        }
+    }
     glutPostRedisplay();
 }
 
-FILE * fout = NULL;
-FILE * fin = NULL;
-
-float teapotX=0, teapotY=0;
-float angle=0, angle2=0, angle3=0;
 void display()
 {
 
@@ -34,10 +47,10 @@ void display()
         glScalef(0.2,0.2,0.2);
          if(body==NULL)
             {
-               head = glmReadOBJ("model/head.obj");
-              body = glmReadOBJ("model/body.obj");
-             uparmR = glmReadOBJ("model/uparmR.obj");
-             lowarmR = glmReadOBJ("model/lowarmR.obj");
+                head = glmReadOBJ("model/head.obj");
+                body = glmReadOBJ("model/body.obj");
+                uparmR = glmReadOBJ("model/uparmR.obj");
+                lowarmR = glmReadOBJ("model/lowarmR.obj");
           }
 
         if(ID==0) glColor3f(1,0,0);
@@ -50,39 +63,38 @@ void display()
 
         glPushMatrix();
             glTranslatef(-1.200000, +0.453333, 0);
-            glRotatef(angle,0,0,1);
+            glRotatef(angle[2],0,0,1);
             glTranslatef(1.200000, -0.453333, 0);;
-    
+
             if(ID==2) glColor3f(1,0,0);
             else glColor3f(1,1,1);
             if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
-            glPushMatrix();
 
+            glPushMatrix();
                 glTranslatef(-1.959999, +0.113333, 0);
-                glRotatef(angle,0,0,1);
-                glTranslatef(-1.959999, -0.113333, 0);
+                glRotatef(angle[3],0,0,1);
+                glTranslatef(1.959999, -0.113333, 0);
+
                 if(ID==3) glColor3f(1,0,0);
                 else glColor3f(1,1,1);
                 if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
-    
             glPopMatrix();
         glPopMatrix();
-    
     glPopMatrix();
+
     glColor3f(0,1,0);
     glutSolidTeapot( 0.02 );
     glutSwapBuffers();
 
 }
 
-int oldX=0, oldY=0;
 void motion(int x ,int y)
 {
     teapotX +=(x - oldX)/150.0;
     teapotY -=(y -  oldY)/150.0;
+    angle[ID] += (x - oldX);
     oldX = x;
     oldY = y;
-    angle = x;
     printf("glTranslatef(%f, %f ,0);\n",teapotX, teapotY);
     ///glutPostRedisplay();
     glutPostRedisplay();
@@ -92,7 +104,7 @@ void mouse(int button, int state, int x, int y)
     if(state==GLUT_DOWN){
         oldX = x;
         oldY = y;
-        angle = x;
+        angle[ID] = x;
         ///teapotX = (x-150)/150.0;
         ///teapotY = (150-y)/150.0;
         ///if(fout==NULL) fout = fopen("file4.txt","w");
